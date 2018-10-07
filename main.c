4cca07b23293db67586eb3b6a547fbba9725293a
AriDaniele <arielandreas.daniele@students.salle.url.edu>
2018-10-07 14:19:30 +0200

    main.c modificat
    creacio de la llista i insercio dalguns elements

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "llista.h"
+
+int int main() {
+  int opcio, i, e;
+  char sortir = 'k';
+  Llista l;
+  l = LLISTA_crea();
+  printf("Llista creada\n\n");
+  printf("Ara introduirem 5 enters aleatoris a la llista\n");
+
+  e = 4;
+  LLISTA_insereixOrdenat(&l,e);
+  printf("4 introduit\n");
+  e = -3;
+  LLISTA_insereixOrdenat(&l,e);
+  printf("-3 introduit\n");
+  e = 9;
+  LLISTA_insereixOrdenat(&l,e);
+  printf("9 introduit\n");
+  e = 2;
+  LLISTA_insereixOrdenat(&l,e);
+  printf("2 introduit\n");
+  e = 18;
+  LLISTA_insereixOrdenat(&l,e);
+  printf("18 introduit\n");
+
+
+  printf("\nA continuació la llista quedarà esborrada i es sortirà del programa\n");
+
+  LLISTA_destrueix(&l);
+
+  return 0;
+}
